2f028a2bcbf67536ee7c3f61543f087acf72b6d2
enridaga <enricodaga@gmail.com>
2018-06-17 11:56:53 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+require_once ('mvctools.inc');
+
+function controller($controller){
+	require_once ('controllers/' . $controller . '.inc');
+	return ucfirst($controller . 'Controller');
+}
+function route($controller = 'default', $action = 'index', $params = array()){
+	$parameters = '';
+	foreach($params as $k => $v){
+		$parameters .= '&' . $k .'=' . $v ;
+	}
+	return '?controller=' . $controller . '&action=' . $action . $parameters;
+}
+function router(){
+	$params = array_merge($_GET, $_POST );
+	$controller = 'default';
+	if(isset($params['controller'])){
+		$controller = $params['controller'];
+	}else{
+		$params['controller'] = $controller;
+	}
+	$action = 'index';
+	if(isset($params['action'])){
+		$action = $params['action'];
+	}else{
+		$params['action'] = $action;
+	}
+
+	return array(
+			'controller' => $controller ,
+			'action' => $action,
+			'parameters' => $params );
+}
+
+function main(){
+	require_once 'http.inc';
+	require_once 'include.inc';
+	try{
+		$routing = router();
+		$action = $routing['action'];
+		$controllerClass = controller($routing['controller']);
+		$controller = new $controllerClass (array('parameters' => $routing['parameters']));
+		$controller->run();
+	}catch(Exception $e){
+		error_internal_server_error($e->getMessage());
+	}
+}
+main();
